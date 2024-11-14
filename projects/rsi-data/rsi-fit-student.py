6896178631a58a6f5ff198e176d50070206dd1c4
Jason Forsyth <forsy2jb@jmu.edu>
2024-11-14 11:36:30 -0500

    Update question instructions

--- projects/rsi-data/rsi-fit-student.py
+++ projects/rsi-data/rsi-fit-student.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 """
 Preamble: Load data from source CSV file
 """
+
 ### YOUR CODE HERE
 
 """
@@ -22,7 +23,7 @@ print('-----Question 1-----')
 Question 2: Conduct a Chi2 Goodness of Fit Test for each dataset to test whether the data is a good fit
 for the derived normal distribution. Clearly print out the p-value, chi2 stat, and an indication of whether it is 
 a fit or not. Do this for both acceleration and force plate distributions. It is suggested to generate 9 bins between 
-[0,2), with the 10th bin encompassing [2,inf). An alpha=0.05 is suitable for these tests.
+[0,2), add append -inf and +inf to both ends of the bins. An alpha=0.05 is suitable for these tests.
 """
 print('\n\n-----Question 2-----')
 
@@ -40,13 +41,14 @@ Force Plate
 """
 Question 3: Perform a t-test to determine whether the RSI means for the acceleration and force plate data are equivalent 
 or not. Clearly report the p-value for the t-test and make a clear determination as to whether they are equal or not.
+An alpha=0.05 is suitable for these tests.
 """
 print('\n\n-----Question 3-----')
 
 ### YOUR CODE HERE
 
 """
-Question 4 (Bonus): Calculate the RSI Error for the dataset where error is expressed as the difference between the 
+Question 4: Calculate the RSI Error for the dataset where error is expressed as the difference between the 
 Force Plate RSI measurement and the Accelerometer RSI measurement. Fit this error distribution to a normal curve and 
 plot a histogram of the data on the same plot showing the fitted normal curve. Include appropriate labels, titles, and 
 legends. The default binning approach from matplot lib with 16 bins is sufficient.
